96b1c5fcbac1734e5686655f21214824939d4476
docscodes <docscodes@gmail.com>
2024-09-26 14:15:58 -0400

    fix json_schema_extra

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class Todo(BaseModel):
     return cls(item=item)
 
   class Config:
-    schema_extra = {
+    json_schema_extra = {
         "example": {
             "id": 1,
             "item": "Example schema!"
@@ -25,7 +25,7 @@ class TodoItem(BaseModel):
   item: str
 
   class Config:
-    schema_extra = {
+    json_schema_extra = {
         "example": {
             "item": "Read the next chapter of the book"
         }
